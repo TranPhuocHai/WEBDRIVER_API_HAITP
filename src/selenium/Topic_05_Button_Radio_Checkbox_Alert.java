e0e2d30fc6aad93bf489a7ee3be229bbe0367d26
Hai Tran <hai.gsd@gmail.com>
2019-03-20 14:00:48 +0700

    Topic_05: Button/RadioButton/Checkbox/Alert: Finished

--- src/selenium/Topic_05_Button_Radio_Checkbox_Alert.java
+++ src/selenium/Topic_05_Button_Radio_Checkbox_Alert.java
@@ -2,6 +2,7 @@ package selenium;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -25,7 +26,7 @@ public class Topic_05_Button_Radio_Checkbox_Alert {
 
 	}
 
-	@Test (enabled = false)
+	@Test (enabled = true)
 	public void TC_01_Button_JSclick() {
 		
 		driver.get("http://live.guru99.com/");
@@ -38,16 +39,16 @@ public class Topic_05_Button_Radio_Checkbox_Alert {
 		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/create/");
 		
 	}
-	
-	
+		
 	@Test (enabled = true)
 	public void TC_02_KendoUi_CheckBox() throws Exception {
 		driver.get("https://demos.telerik.com/kendo-ui/styling/checkboxes");
 		
-		WebElement DualZoneCheckbox = driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input"));
+		By DualZoneXpath = By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input");
+		WebElement DualZoneCheckbox = driver.findElement(DualZoneXpath);
 		
 		jsExecutor.executeScript("arguments[0].click();", DualZoneCheckbox);	
-		Assert.assertTrue(DualZoneCheckbox.isSelected());
+		Assert.assertTrue(isElementSelected((DualZoneXpath)));
 		Thread.sleep(2000);
 		
 		jsExecutor.executeScript("arguments[0].click();", DualZoneCheckbox);	
@@ -56,8 +57,76 @@ public class Topic_05_Button_Radio_Checkbox_Alert {
 		
 	}
 	
-
-
+	@Test (enabled = true)
+	public void TC_03_KendoUi_RadioButton() throws Exception {
+		driver.get("https://demos.telerik.com/kendo-ui/styling/radios");
+		
+		By expectedRadioXpath = By.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input");
+		WebElement expectedRadioButton = driver.findElement(expectedRadioXpath);
+		
+		jsExecutor.executeScript("arguments[0].click();", expectedRadioButton);	
+		Assert.assertTrue(expectedRadioButton.isSelected());
+		Thread.sleep(2000);
+		
+	}
+	
+	@Test (enabled = true)
+	public void TC_04_Accept_Confirm_Prompt_Alert() throws Exception {
+		
+		//Accept Alert
+		driver.get("https://daominhdam.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		Thread.sleep(1500);
+		Alert acceptAlert = driver.switchTo().alert();
+		String acceptAlertText = acceptAlert.getText();
+		Assert.assertEquals(acceptAlertText, "I am a JS Alert");
+		acceptAlert.accept();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@id='result' and text()='You clicked an alert successfully ']")).isDisplayed());
+		Thread.sleep(1500);
+		
+		//Confirm Alert
+		driver.navigate().refresh();
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		Thread.sleep(1500);
+		Alert confirmAlert = driver.switchTo().alert();
+		String confirmAlertText = confirmAlert.getText();
+		Assert.assertEquals(confirmAlertText, "I am a JS Confirm");
+		confirmAlert.dismiss();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@id='result' and text()='You clicked: Cancel']")).isDisplayed());
+		
+		
+		//Prompt Alert
+		driver.navigate().refresh();	
+		String inputText = "Tran Phuoc Hai";		
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		Thread.sleep(1500);
+		Alert promptAlert = driver.switchTo().alert();
+		String promptAlertText = promptAlert.getText();
+		Assert.assertEquals(promptAlertText, "I am a JS prompt");			
+		promptAlert.sendKeys(inputText);		
+		Thread.sleep(2000);
+		promptAlert.accept();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@id='result' and text()='You entered: "+ inputText +"']")).isDisplayed());
+		
+				
+	}
+		
+	@Test (enabled = true)
+	public void TC_05_Authentication_Alert() throws Exception {
+		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials')]")).isDisplayed());
+		Thread.sleep(3000);
+	}
+		
+	public boolean isElementSelected(By byValue) {
+		  if(driver.findElement(byValue).isSelected()){
+			  System.out.println("Element [" + byValue + "] is selected!");
+			  return true;
+		  } else {
+			 System.out.println("Element [" + byValue + "] is de-selected!");
+			 return false;
+		  }		  
+	  }
 	
 	@AfterTest
 	public void afterTest() {
