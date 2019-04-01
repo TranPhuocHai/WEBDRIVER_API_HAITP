750dcf020a4abcd475d4afd14d3eee45cc09ebca
Hai Tran <hai.gsd@gmail.com>
2019-04-01 17:04:11 +0700

    Topic_10: Doing

--- src/selenium/Topic_10_Javascript_Executor.java
+++ src/selenium/Topic_10_Javascript_Executor.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
